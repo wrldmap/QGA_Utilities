2ddd03e56a17264bc74039ff0c3f1cf7c66f90c8
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-09-22 14:34:10 -0500

    Import mod base.

---
+++ src/client/java/gg/wrldmap/qgautilities/QGAUtilitiesClient.java
@@ -0,0 +1,10 @@
+package gg.wrldmap.qgautilities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QGAUtilitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
